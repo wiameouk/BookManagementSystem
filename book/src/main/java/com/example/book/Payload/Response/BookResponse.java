2a3ddca82e8f1778d630eff53f1f648252ca3236
wiameouk <wiamoukaili7@gmail.com>
2024-11-15 10:51:59 +0100

    update

--- book/src/main/java/com/example/book/Payload/Response/BookResponse.java
+++ book/src/main/java/com/example/book/Payload/Response/BookResponse.java
@@ -8,6 +8,5 @@ public record BookResponse(
     Date publicationDate,
     double price,
     String descreption
-
 ) {  
 }
--- book/src/main/java/com/example/book/Payload/Response/CategoryResponse.java
+++ book/src/main/java/com/example/book/Payload/Response/CategoryResponse.java
@@ -3,8 +3,6 @@ package com.example.book.Payload.Response;
 public record CategoryResponse(
     String id,
     String name ,
-    String decription
-   
-    
+    String decription   
 ) {   
 }
